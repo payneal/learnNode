cce97392ea3e7767586b34ff11822b9e0d8c3ffe
Ali Payne <ali.payne@zipscene.com>
2016-03-10 14:03:38 -0500

    finished lecture 33

--- section5/lecture33/app.js
+++ section5/lecture33/app.js
@@ -1,1 +1,14 @@
- 
+ var Emitter = require('./emitter');
+
+var emtr = new Emitter();  
+
+emtr.on('greet', function(){ 
+    console.log('somewhere, someone said hello'); 
+}); 
+
+emtr.on('greet', function(){
+    console.log('a greeting happened'); 
+});
+
+console.log('hello'); 
+emtr.emit('greet'); 
